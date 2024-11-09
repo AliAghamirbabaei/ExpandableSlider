47863014cf1f6788170fc724fa7c0443c7f34647
Ali Aghamirbabaei <Ali.Aghamirbabaei@gmail.com>
2024-11-09 20:25:09 +0330

    Initial Commit

---
+++ ExpandableSlider/ExpandableSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableSliderApp.swift
+//  ExpandableSlider
+//
+//  Created by Ali Aghamirbabaei on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
